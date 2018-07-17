98cb5802558266020d73b756e908100897972c4c
Bejofo <32919220+Bejofo@users.noreply.github.com>
2018-07-17 23:42:40 +0800

    RL patch 1
    Color fix.
    Tileset upgraded.
    Other design changes.

--- rl/scripts/content.js
+++ rl/scripts/content.js
@@ -1,8 +1,39 @@
-var content ={
-  Terrain:{
-    floor: new Terrain('floor', '', '#afafaf','#afafaf', false, false),
-    wall: new Terrain ('wall', '#', '#FFFFFF','#000000', true, true),
-    odoor:new Terrain('odoor','-','#FFFFFF','gray',false,false),
-    cdoor: new Terrain('cdoor', '+', '#FFFFFF', '#000000', true, true)
-  }
+var content = {
+	Monsters: {
+		rat: new Monster({
+			id: 'rat',
+			icon: 'r',
+			fcolor: '#ffffff'
+		})
+	},
+	Terrain: {
+		floor: new Terrain({
+			id: 'floor',
+			icon: ' ',
+			fcolor: '#2d2200',
+			breaksLineOfSight: false,
+			solid: false
+		}),
+		wall: new Terrain({
+			id: 'wall',
+			icon: '#',
+			fcolor: '#afafaf',
+			breaksLineOfSight: true,
+			solid: true
+		}),
+		odoor: new Terrain({
+			id: 'odoor',
+			icon: '-',
+			fcolor: '#ffffff',
+			breaksLineOfSight: false,
+			solid: false
+		}),
+		cdoor: new Terrain({
+			id: 'cdoor',
+			icon: '+',
+			fcolor: '#ffffff',
+			breaksLineOfSight: true,
+			solid: true
+		})
+	}
 }
--- rl/scripts/levelgen.js
+++ rl/scripts/levelgen.js
@@ -6,21 +6,25 @@ class Level {
 		this.height = height;
 		this.digger = new ROT.Map.Digger(this.width, this.height);
 		this.map = [];
+		this.bg = '#2d2200';
 		for (var i = 0; i < this.width; i++) {
 			this.map[i] = [];
+			for (var z = 0; z < this.height; z++) {
+				this.map[i][z] = new Tile();
+			}
 		}
 		this.digger.create(function(x, y, ele) {
 			if (ele) {
-				a.map[x][y] =  content.Terrain.wall;
+				a.map[x][y].Terrain = content.Terrain.wall;
 			} else {
-				a.map[x][y] = content.Terrain.floor;
+				a.map[x][y].Terrain = content.Terrain.floor;
 			}
 		})
 		var room = this.digger._rooms;
 		for (var i = 0; i < room.length; i++) {
 			for (var k = 0; k < Object.keys(room[i]._doors).length; k++) {
 				var coordinates = Object.keys(room[i]._doors)[k].split(',');
-				a.map[coordinates[0]][coordinates[1]] = content.Terrain.cdoor;
+				a.map[coordinates[0]][coordinates[1]].Terrain = content.Terrain.cdoor;
 			}
 		}
 		this.spawnPoint = [Math.floor(Math.random() * (this.digger._rooms[0]._x2 - this.digger._rooms[0]._x1 + 1)) + this.digger._rooms[0]._x1, Math.floor(Math.random() * (this.digger._rooms[0]._y2 - this.digger._rooms[0]._y1 + 1)) + this.digger._rooms[0]._y1]; // Gets digger._rooms[0] and choose a random point in the room to spawn in
@@ -29,30 +33,57 @@ class Level {
 		var a = this;
 		var lightPasses = function(x, y) {
 			try {
-				return !a.map[x][y].breaksLineOfSight;
+				return !a.map[x][y].Terrain.breaksLineOfSight;
 			} catch (err) {
 				return false;
 			}
 		}
 		var fov = new ROT.FOV.PreciseShadowcasting(lightPasses);
-    display.clear();
+		display.clear();
 		fov.compute(player.coordinates[0], player.coordinates[1], 10, function(x, y, r, visibility) {
 			var dx = x - player.coordinates[0] + 29;
 			var dy = y - player.coordinates[1] + 14;
-			display.draw(dx, dy, a.map[x][y].icon,ROT.Color.toRGB(a.map[x][y].fcolor),ROT.Color.toRGB(a.map[x][y].bcolor),);
+			display.draw(dx, dy, a.map[x][y].icon, ROT.Color.toHex(a.map[x][y].fColor), a.bg);
 			display._data[dx + ',' + dy].push([x, y])
 		});
-		display.draw(29, 14, "@");
+		display.draw(29, 14, "@", '#ffffff', '#2d2200');
+	}
+}
+class Monster {
+	constructor(obj) {
+		this.id = obj.id;
+		this.icon = obj.icon;
+		this.fcolor = ROT.Color.fromString(obj.fcolor);
+	}
+}
+
+class Tile {
+	constructor() {
+		this.Terrain = null;
+		this.Monster = null;
+	}
+	get icon() {
+		if (this.Monster != null) {
+			return this.Monster.icon
+		} else {
+			return this.Terrain.icon
+		}
+	}
+	get fColor() {
+		if (this.Monster != null) {
+			return this.Monster.fcolor
+		} else {
+			return this.Terrain.fcolor
+		}
 	}
 }
 
 class Terrain {
-	constructor(id, icon, fcolor,bcolor, breaksLineOfSight, solid) {
-		this.id = id;
-		this.icon = icon;
-		this.fcolor = ROT.Color.fromString(fcolor);
-    this.bcolor = ROT.Color.fromString(bcolor)
-		this.breaksLineOfSight = breaksLineOfSight;
-		this.solid = solid;
+	constructor(obj) {
+		this.id = obj.id;
+		this.icon = obj.icon;
+		this.fcolor = ROT.Color.fromString(obj.fcolor);
+		this.breaksLineOfSight = obj.breaksLineOfSight;
+		this.solid = obj.solid;
 	}
 }
