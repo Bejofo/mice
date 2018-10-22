1c2bd4a2edc5b5f7ee08e891a19204625d587b40
Bejofo <32919220+Bejofo@users.noreply.github.com>
2018-10-22 13:37:45 +0800

    Formmating

--- progress/js.js
+++ progress/js.js
@@ -1,71 +1,63 @@
 // ------------- Time functions --------------------
-function th()
-{
-var d = new Date();
-return d.getHours();
+function th() {
+  var d = new Date();
+  return d.getHours();
 }
-function tm()
-{
-var d = new Date();
-return d.getMinutes();
+function tm() {
+  var d = new Date();
+  return d.getMinutes();
 }
-function ts()
-{
-var d = new Date();
-return d.getSeconds();
+function ts() {
+  var d = new Date();
+  return d.getSeconds();
 }
 // --------------------------------------------------
-function updateBar(p,z) {
-  	if (p <= 3)
-    {
-    document.getElementById(z).style = "width:"+5+"%";
-    document.getElementById(z).innerHTML = Math.ceil(p*10)/10+"%"
-   	} else
-    {
-    document.getElementById(z).style = "width:"+p+"%";
-    document.getElementById(z).innerHTML = Math.ceil(p*10)/10+"%"
-    }
+function updateBar(p, z) {
+  if (p <= 3) {
+    document.getElementById(z).style = "width:" + 5 + "%";
+    document.getElementById(z).innerHTML = Math.ceil(p * 10) / 10 + "%"
+  } else {
+    document.getElementById(z).style = "width:" + p + "%";
+    document.getElementById(z).innerHTML = Math.ceil(p * 10) / 10 + "%"
+  }
 }
 function getPeriod() {
-if(timeToSecs(7,45,0)<=timeToSecs(th(),tm(),ts())&&timeToSecs(th(),tm(),ts())<=timeToSecs(9,5,0)){return 1;}
-if(timeToSecs(9,15,0)<=timeToSecs(th(),tm(),ts())&&timeToSecs(th(),tm(),ts())<=timeToSecs(10,35,0)){return 2;}
-if(timeToSecs(11,25,0)<=timeToSecs(th(),tm(),ts())&&timeToSecs(th(),tm(),ts())<=timeToSecs(12,45,0)){return 3;}
-if(timeToSecs(12,50,0)<=timeToSecs(th(),tm(),ts())&&timeToSecs(th(),tm(),ts())<=timeToSecs(14,10,0)){return 4;}
-if(timeToSecs(14,15,0)<=timeToSecs(th(),tm(),ts())&&timeToSecs(th(),tm(),ts())<=timeToSecs(15,35,0)){return 5;}
-return 0;
+  if (timeToSecs(7, 45, 0) <= timeToSecs(th(), tm(), ts()) && timeToSecs(th(), tm(), ts()) <= timeToSecs(9, 5, 0)) { return 1; }
+  if (timeToSecs(9, 15, 0) <= timeToSecs(th(), tm(), ts()) && timeToSecs(th(), tm(), ts()) <= timeToSecs(10, 35, 0)) { return 2; }
+  if (timeToSecs(11, 25, 0) <= timeToSecs(th(), tm(), ts()) && timeToSecs(th(), tm(), ts()) <= timeToSecs(12, 45, 0)) { return 3; }
+  if (timeToSecs(12, 50, 0) <= timeToSecs(th(), tm(), ts()) && timeToSecs(th(), tm(), ts()) <= timeToSecs(14, 10, 0)) { return 4; }
+  if (timeToSecs(14, 15, 0) <= timeToSecs(th(), tm(), ts()) && timeToSecs(th(), tm(), ts()) <= timeToSecs(15, 35, 0)) { return 5; }
+  return 0;
 }
-function timeToSecs(a,b,c)
-{
-var a = a * 60 * 60;
-var b = b * 60;
-return a + b + c;
+function timeToSecs(a, b, c) {
+  var a = a * 60 * 60;
+  var b = b * 60;
+  return a + b + c;
 }
-function partOfClass(wc)
-{
-if (wc == 1) {
-return (timeToSecs(th(),tm(),ts()) - timeToSecs(7,45,0)) / 4800
+function partOfClass(wc) {
+  if (wc == 1) {
+    return (timeToSecs(th(), tm(), ts()) - timeToSecs(7, 45, 0)) / 4800
+  }
+  if (wc == 2) {
+    return (timeToSecs(th(), tm(), ts()) - timeToSecs(9, 15, 0)) / 4800
+  }
+  if (wc == 3) {
+    return (timeToSecs(th(), tm(), ts()) - timeToSecs(11, 25, 0)) / 4800
+  }
+  if (wc == 4) {
+    return (timeToSecs(th(), tm(), ts()) - timeToSecs(12, 50, 0)) / 4800
+  }
+  if (wc == 5) {
+    return (timeToSecs(th(), tm(), ts()) - timeToSecs(14, 15, 0)) / 4800
+  }
+  if (wc == 0) {
+    return 1;
+  }
 }
-if (wc == 2) {
-return (timeToSecs(th(),tm(),ts()) - timeToSecs(9,15,0)) / 4800
+function update() {
+  updateBar(partOfClass(getPeriod()) * 100, 1);
 }
-if (wc == 3) {
-return (timeToSecs(th(),tm(),ts()) - timeToSecs(11,25,0)) / 4800
-}
-if (wc == 4) {
-return (timeToSecs(th(),tm(),ts()) - timeToSecs(12,50,0)) / 4800
-}
-if (wc == 5) {
-return (timeToSecs(th(),tm(),ts()) - timeToSecs(14,15,0)) / 4800
-}
-if (wc == 0){
-return 1;
-}
-}
-function update()
-{
-updateBar(partOfClass(getPeriod())*100,1);
-}
-setInterval(function(){ update() }, 1000);
+setInterval(function () { update() }, 1000);
 console.log("It is now period:" + getPeriod());
 /*
 th(){
